b732b2dba2d820a5bb6f2d83b127250288895631
Hennadii <gennadii.lag@gmail.com>
2021-07-20 22:05:06 +0300

    all right

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,6 +1,25 @@
 #include <algorithm>
 #include <iostream>
+#include <cstring>
+#include <cmath>
+/*
+ Заімплементувати просту (але робочу) реалізацію для динамічного масива символів, котрий надаватиме ітератори для роботи з алгоритмами STL.
 
+Клас повинен підтримувтаи :
+1) пуста ініціалізація (дефолтний конструктор)
+2) ініціалізація з параметром
+3) копіювання, мув + оператори копії та муву
+4) повинен динамічно алокувати пам'ять для масиву символів (та видаляти її)
+5) підтримувати наступний функціонал:
+      5.1 operator []
+      5.2 at()
+      5.3 append
+      5.4 size()
+      5.5 capacity()
+      5.6 erase(uint32_t)
+      5.7 +=, +
+6) працювати з std::sort
+ */
 using namespace std;
 
 //https://question-it.com/questions/2117222/klass-shablona-dinamicheskogo-massiva-problema-s-funktsiej-ostream-operator-friend
@@ -8,34 +27,39 @@ template<class T>
 class SymbolArray;
 
 template<class T>
-ostream &operator<<(ostream &out, const SymbolArray<T> &obj);
+wostream &operator<<(wostream &out, const SymbolArray<T> &obj);
 
 template<class T>
 class SymbolArray {
 private:
     uint32_t _capacity;
     uint32_t _count;
-    T* _array;
+    T *_array;
 
-    void resize() {
-        T *newArray = new T[_capacity];
-        for (int i = 0; i < _count; i++) {
-            newArray[i] = _array[i];
-        }
+    void resize(uint32_t _new_capacity = 10) {
+        uint32_t _old_capacity = _capacity;
+        _capacity += _new_capacity;
+        T *_tmp_array = new T[_old_capacity];
+        memcpy(_tmp_array, _array, _old_capacity * sizeof(T));
         delete[] _array;
-        _array = newArray;
+        _array = new T[_capacity];
+        memset(_array, 0, _capacity * sizeof(T));
+        memcpy(_array, _tmp_array, _old_capacity * sizeof(T));
+        cout << "DEBUG: call resize. New capacity :" << capacity() << endl;
     }
 
 public:
+    class Iterator;
 
-    SymbolArray(uint32_t _capacity = 10) : _capacity(_capacity) {
+    explicit SymbolArray(uint32_t _capacity = 10) : _capacity(_capacity) {
         _count = 0;
-        _array = new T[_capacity]();
-
+        _array = new T[_capacity];
+        memset(_array, 0, _capacity * sizeof(T));
+        cout << "DEBUG: constructor: capacity = " << capacity() << endl;
     }
 
     SymbolArray(const SymbolArray &other) {
-        const auto newSize = std::min(sizeof(other.array), sizeof(_array));
+        const auto newSize = std::min(sizeof(T) * other._capacity, sizeof(T) * _capacity);
         memcpy(_array, other._array, newSize);
         _capacity = other._capacity;
         _count = other._count;
@@ -49,17 +73,15 @@ public:
         delete[] _array;
     }
 
-    class Iterator;
-
-    Iterator begin() {
+    Iterator begin() const {
         return Iterator{_array};
     }
 
-    Iterator end() {
-        return {_array + size()};
+    Iterator end() const {
+        return Iterator{_array + size()};
     }
 
-    uint32_t size() {
+    uint32_t size() const {
         return _count;
     }
 
@@ -76,7 +98,6 @@ public:
 
     void append(const T _elem) {
         if (_count == _capacity) {
-            _capacity += 10;
             resize();
         }
         _array[_count] = _elem;
@@ -84,42 +105,60 @@ public:
     }
 
     void erase(uint32_t index) {
-        uint32_t _index = index;
-        if (_index >= _count) {
+        if (index >= _count) {
             throw runtime_error{"Index is out of range"};
         }
-        if (_index == _count - 1) {
+        if (index == _count - 1) {
             _count--;
             return;
         }
-        T *newArray = new T[_capacity];
-        for (uint32_t i = 0; i < _index; i++) {
-            newArray[i] = _array[i];
-        }
-        _index++;
-        for(uint32_t i = _index; i < _count; i++) {
-            newArray[i] = _array[i];
-        }
-        _count --;
-        delete[] _array;
-        _array = newArray;
+
+        T *_tmp_array = new T[_capacity];
+        memcpy(_tmp_array, _array, _capacity * sizeof(T));
+        memcpy(_array, _tmp_array, (index - 1) * sizeof(T));
+        memcpy(_array + (index - 1), _tmp_array + index, (_capacity - index) * sizeof(T));
+        _count--;
+        delete[] _tmp_array;
     }
 
     T &operator[](uint32_t index) {
         return at(index);
     }
 
-    friend ostream & operator << <>(ostream & out, const SymbolArray<T>& obj);
+    SymbolArray<T> operator+(SymbolArray<T> &_symbol_array) {
+        uint32_t _new_capacity = ceil(double (_count + _symbol_array._count) / 10) * 10;
+        SymbolArray<T> _new_symbol_array(_new_capacity);
+        memcpy(_new_symbol_array._array, _array, _count * sizeof(T));
+        memcpy(_new_symbol_array._array + _count, _symbol_array._array, _symbol_array._count * sizeof(T));
+        _new_symbol_array._count = _count + _symbol_array._count;
+        return _new_symbol_array;
+    }
+
+    void operator+=(SymbolArray<T> &_symbol_array) {
+        uint32_t _new_capacity = ceil(double (_count + _symbol_array._count) / 10) * 10;
+        if (_new_capacity >= _capacity) {
+            resize(_new_capacity - _capacity);
+        }
+        memcpy(_array + _count, _symbol_array._array, _symbol_array._count * sizeof(T));
+        _count +=_symbol_array._count;
+    }
+
+    friend wostream &operator
+    <<<>(
+    wostream &out,
+    const SymbolArray<T> &obj
+    );
 
     class Iterator {
     private:
         T *_currElement;
     public:
-        using iterator_category = random_access_iterator_tag;
-        using difference_type = ptrdiff_t;
+
+        using iterator_category = std::random_access_iterator_tag;
+        using difference_type = std::ptrdiff_t;
         using value_type = T;
-        using pointer = T *;
-        using reference = T &;
+        using pointer = T *;  // or also value_type*
+        using reference = T &;  // or also value_type&
 
         Iterator() = default;
 
@@ -133,28 +172,36 @@ public:
             return Iterator{_currElement + index};
         }
 
-        Iterator operator++() const {
+        uint32_t operator-(const Iterator &other) const {
+            return _currElement - other._currElement;
+        }
+
+        uint32_t operator+(const Iterator &other) const {
+            return _currElement + other._currElement;
+        }
+
+        Iterator operator++() {
             return Iterator{_currElement++};
         }
 
-        Iterator operator--() const {
+        Iterator operator--() {
             return Iterator{_currElement--};
         }
 
-        bool operator>(const Iterator *other) const {
-            return _currElement > other->_currElement;
+        bool operator>(const Iterator &other) {
+            return _currElement > other._currElement;
         }
 
-        bool operator<(const Iterator *other) const {
-            return _currElement < other->_currElement;
+        bool operator<(const Iterator &other) {
+            return _currElement < other._currElement;
         }
 
-        bool operator==(const Iterator *other) const {
-            return _currElement = other->_currElement;
+        bool operator==(const Iterator &other) {
+            return _currElement == other._currElement;
         }
 
-        bool operator!=(const Iterator *other) const {
-            return _currElement != other->_currElement;
+        bool operator!=(const Iterator &other) {
+            return _currElement != other._currElement;
         }
 
         T &operator*() {
@@ -164,22 +211,50 @@ public:
 };
 
 template<class T>
-ostream &operator<<(ostream &out, const SymbolArray<T> &obj) {
-    for(auto elem : obj._array) {
-        out<<elem;
+wostream &operator<<(wostream &out, const SymbolArray<T> &obj) {
+    for (auto elem : obj) {
+        out << elem;
     }
-    out<<endl;
     return out;
 }
 
 
 int main() {
-    SymbolArray<wchar_t> symbolArray(10);
+    SymbolArray<char> symbolArray(10);
+    //test append
     symbolArray.append('a');
     symbolArray.append('b');
     symbolArray.append('c');
+    symbolArray.append('d');
+    symbolArray.append('e');
+    symbolArray.append('j');
+    symbolArray.append('i');
+    symbolArray.append('h');
+    symbolArray.append('g');
+    symbolArray.append('f');
 
-    cout<<symbolArray<<endl;
+    wcout << symbolArray << endl;
+    //test resize
+    symbolArray.append('k');
+    wcout << symbolArray << endl;
+    //test erase
+    symbolArray.erase(4);
+    wcout << symbolArray << endl;
+
+    //test std
+    std::sort(symbolArray.begin(), symbolArray.end());
+    wcout << symbolArray << endl;
+
+    //test += override
+    SymbolArray<char> symbolArray0(10);
+    symbolArray0.append('1');
+    symbolArray0.append('9');
+    symbolArray+=symbolArray0;
+    wcout << symbolArray << endl;
+
+    //test + override
+    SymbolArray<char> ss = symbolArray + symbolArray0;
+    wcout << ss << endl;
+
     return 0;
 }
-
