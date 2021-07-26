57abfed15aab5e6e93c134ea0b83719cae4b79be
Hennadii <gennadii.lag@gmail.com>
2021-07-26 23:32:34 +0300

    HW4

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <utility>
 #include <stdio.h>
 
 using namespace std;
@@ -17,13 +18,13 @@ public:
         this->error.append(strerror(errno));
     }
 
-    FileException(string error) : error(error){}
+    explicit FileException(string error) : error(std::move(error)){}
 
     const char *what() const noexcept { return error.c_str(); } // C++11 and higher
 private:
     string error;
 };
-
+template<class T>
 class File {
 public:
     File(string fileName, AccessMode accessMode) : accessMode(accessMode) {
@@ -51,20 +52,22 @@ public:
         }
     }
 
-    void write(char *buffer, size_t size, long position = 0) {
+    void write(T *buffer, size_t size) {
         if (accessMode == READ) {
             throw FileException("File exception: File opened read only");
         }
-        fseek(file, position, SEEK_SET);
-        fwrite(buffer, sizeof(char), size, file);
+        fseek(file, 0, SEEK_END);
+        fwrite(buffer, sizeof(T), size, file);
     }
 
-    size_t read(char *buffer, size_t size, long position = 0) {
+    size_t read(T *buffer, size_t size, long offset = 0) {
         if (accessMode == WRITE) {
             throw FileException("File exception: File opened write only");
         }
-        fseek(file, position, SEEK_SET);
-        return fread(buffer, sizeof(char), size, file);
+        if (fseek(file, offset, SEEK_SET) != 0) {
+            throw FileException("File exception: File seek error");
+        }
+        return fread(buffer, sizeof(T), size, file);
     }
 
 private:
@@ -75,12 +78,11 @@ private:
 int main() {
     char * buffer = new char [10];
     try {
-        File file("D:\\1C\\buffer.txt", READ_WRITE);
-        file.read(buffer, 10);
+        File<char> file("D:\\1C\\buffer.txt", READ_WRITE);
+        file.read(buffer, 11, 0);
         cout<<buffer<<endl;
         string writeBuffer = "write_test";
-        file.write(const_cast<char *>(writeBuffer.c_str()), writeBuffer.length(), 0);
-        file.read(buffer, 10, 0);
+        file.write(const_cast<char *>(writeBuffer.c_str()), writeBuffer.length());
         cout<<buffer<<endl;
     }
     catch (FileException e) {
