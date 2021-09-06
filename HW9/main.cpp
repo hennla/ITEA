4fbc84aa3296f42b6de0c8d0864ed06ddc62d4e5
Hennadii <gennadii.lag@gmail.com>
2021-09-06 12:30:05 +0300

    HW9

---
+++ HW9/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
+
+std::condition_variable cv;
+std::mutex cv_m;
+bool print = false;
+bool increment = false;
+
+std::atomic<int> counter{0};
+
+bool interrupted = false;
+
+void produce() {
+    counter++;
+};
+
+void consume() {
+    while (!interrupted) {
+        std::unique_lock<std::mutex> lk(cv_m);
+        cv.wait(lk, [] { return print; });
+        if (counter > 1) std::cout << ", ";
+        std::cout << counter;
+
+        print = false;
+        increment = true;
+        lk.unlock();
+        cv.notify_one();
+    }
+};
+
+int main() {
+    int max_repeat = 30;
+    std::unique_lock<std::mutex> lk(cv_m);
+
+    std::thread consumer_thread(consume);
+
+    for (int i = 0; i < max_repeat; i++) {
+        produce();
+        if (i == max_repeat - 1) interrupted = true;
+        print = true;
+        increment = false;
+        cv.notify_one();
+        cv.wait(lk, [] { return increment; });
+    }
+    consumer_thread.join();
+    return 0;
+}
